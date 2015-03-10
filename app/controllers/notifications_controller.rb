2431bd4a6a8f244dcf284624c572e9a6012aac38
samuel <samuel.whitley89@gmail.com>
2015-03-10 10:25:14 -0400

    remove binding.pry

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,7 +4,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user_from_token!
   def notify
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: '2075187157', to: 'nil', body: 'Learning to send SMS you are.'
+    message = client.messages.create from: '2075187157', to: 'nil', body: ''
     render plain: message.status
   end
  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user_from_token!
 
   def show
-    binding.pry
+    
     @user = User.find(params[:id])
     render json: {:user => @user}
      
