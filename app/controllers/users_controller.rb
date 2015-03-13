96d99d157c689d2d5a8847659a99dd9b357ac5bb
Samuel Whitley <samuel.whitley89@gmail.com>
2015-03-13 19:56:52 -0400

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate_user_from_token!
 
   def show
-    binding.pry
+    
     @user = User.find(params[:id])
-    binding.pry
+    
     render json: {:user => @user}
   end
 
