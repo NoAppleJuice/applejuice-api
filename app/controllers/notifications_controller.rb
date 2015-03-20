4946d79845722a5d6535f7e498bc7fb7df8e22e7
samuel <samuel.whitley89@gmail.com>
2015-03-19 22:41:43 -0400

    changed up notifications

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,12 +9,12 @@ class NotificationsController < ApplicationController
 
 
 
-  def notify
-    @user.phone_numbers.each do |num|
-    @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = @client.messages.create from: '4047248008', to: num.base, body: num.msg
+def notify
+ @notification = current_user.notifications.build(notification_params)
+  if @notification.save
+  @notification.go
   end
- end
+end
 
 # def verify
 #   @client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
@@ -28,8 +28,8 @@ def set_user
   @user = User.find(params[:id])
 end
 
-# def notification_params
-#   params.require(:notification).permit(:message)
-# end
+def notification_params
+  params.require(:notification).permit(:message, :number)
+end
 
 end 
